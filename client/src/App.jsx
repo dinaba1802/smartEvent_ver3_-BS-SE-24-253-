9b9f7b5ea440ecdf579adeb09b9b0cfe2441566f
dinaba1802 <73492440+dinaba1802@users.noreply.github.com>
2024-07-09 20:08:27 +0300

    frontend of project - basic pages (routes)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index:true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
+
 
 const App = () => {
-  return <h1>smartEvent</h1>;
+  return <RouterProvider router={router} />;
 };
-
 export default App;
