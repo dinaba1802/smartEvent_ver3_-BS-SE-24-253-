f9f4b81a1454a2ec8bb046b2b9c67f1e9901488e
dinaba1802 <73492440+dinaba1802@users.noreply.github.com>
2024-07-16 19:22:09 +0300

    Dashboard - frontend

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,20 +1,24 @@
 import { Outlet } from 'react-router-dom';
-
 import Wrapper from '../assets/wrappers/Dashboard';
 import { Navbar, BigSidebar, SmallSidebar } from '../components';
-
 import { useState, createContext, useContext } from 'react';
-const DashboardContext = createContext();
-const Dashboard = () => {
-  // temp
-  const user = { name: 'john' };
+import { checkDefaultTheme } from '../App';
 
+const DashboardContext = createContext();
+
+const DashboardLayout = () => {
+  // temp
+  const user = { name: 'dina' };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
   const toggleDarkTheme = () => {
-    console.log('toggle dark theme');
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
+
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
@@ -52,4 +56,4 @@ const Dashboard = () => {
 
 export const useDashboardContext = () => useContext(DashboardContext);
 
-export default Dashboard;
+export default DashboardLayout;
