cb0792dee097962654c8d3afb37cc9fca269d53f
dinaba1802 <73492440+dinaba1802@users.noreply.github.com>
2024-07-17 20:03:52 +0300

    get all events + post event (requests)

--- server.js
+++ server.js
@@ -3,29 +3,45 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 
-try {
-    const response = await fetch(
-      'https://www.course-api.com/react-useReducer-cart-project'
-    );
-    const cartData = await response.json();
-    console.log(cartData);
-  } catch (error) {
-    console.log(error);
-  }
+  let events = [
+    { id: nanoid(), event_name: 'Amit', event_type: 'Wedding Hall' },
+    { id: nanoid(), event_name: 'Dina', event_type: 'DJ' },
+    { id: nanoid(), event_name: 'Roni', event_type: 'Designer' },
+    //    { id: nanoid(), company: 'Apple', position: 'Back-end' },
+
+  ];
+  
+  //Get all events
+  app.get('/api/v3/events', (req, res) => {
+    res.status(200).json({ events });
+  });
+
+  //Create event
+  app.post('/api/v3/events', (req, res) => {
+    const { event_name, event_type } = req.body;
+    if (!event_name || !event_type) {
+      return res.status(400).json({ msg: 'please provide company and position' });
+    }
+    const id = nanoid(10);
+    const event = { id, event_name, event_type };
+    events.push(event);
+    res.status(200).json({ event });
+  });
+
 
 
 //log the info about our request
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-}
+};
 
 
 app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello Worldd');
-});
+
 
 app.post('/', (req, res) => {
     console.log(req);
