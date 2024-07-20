96d52a3aed75dccd692fc505ae0e0212eb1fd200
amitco6 <amitcoh12@gmail.com>
2024-07-20 16:25:57 +0300

    database connection

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,13 +1,12 @@
-import { nanoid } from 'nanoid';
-import Event from '../models/eventModel.js';
+import { nanoid } from "nanoid";
+import Event from "../models/eventModel.js";
+import { StatusCodes } from 'http-status-codes';
 
-
-
-  let events = [
-    { id: nanoid(), event_name: 'Amit', event_type: 'Wedding Hall' },
-    { id: nanoid(), event_name: 'Dina', event_type: 'DJ' }
-    //    { id: nanoid(), company: 'Apple', position: 'Back-end' },
-  ];
+let events = [
+  { id: nanoid(), event_name: "Amit", event_type: "Wedding Hall" },
+  { id: nanoid(), event_name: "Dina", event_type: "DJ" },
+  //    { id: nanoid(), company: 'Apple', position: 'Back-end' },
+];
 
 export const getAllEvents = async (req, res) => {
   const events = await Event.find({});
@@ -15,13 +14,13 @@ export const getAllEvents = async (req, res) => {
 };
 
 export const createEvent = async (req, res) => {
-    const event = await Event.create( req.body );
-    res.status(StatusCodes.CREATED).json({ event });
-  }
+  const event = await Event.create(req.body);
+  res.status(StatusCodes.CREATED).json({ event });
+};
 
 export const getEvent = async (req, res) => {
-    const { id } = req.params;
-    const event = await Event.findById(id);
+  const { id } = req.params;
+  const event = await Event.findById(id);
   if (!event) {
     return res.status(404).json({ msg: `no event with id ${id}` });
   }
@@ -38,15 +37,19 @@ export const updateEvent = async (req, res) => {
   if (!updatedEvent) {
     return res.status(404).json({ msg: `no event with id ${id}` });
   }
-  res.status(StatusCodes.OK).json({ msg: 'event modified', event: updatedEvent });
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "event modified", event: updatedEvent });
 };
 
-  export const deleteEvent = async (req, res) => {
-    const { id } = req.params;
-    const removedEvent = await Event.findByIdAndDelete(id);
+export const deleteEvent = async (req, res) => {
+  const { id } = req.params;
+  const removedEvent = await Event.findByIdAndDelete(id);
 
   if (!removedEvent) {
     return res.status(404).json({ msg: `no event with id ${id}` });
   }
-  res.status(StatusCodes.OK).json({ msg: 'event deleted', event: removedEvent });
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "event deleted", event: removedEvent });
 };
